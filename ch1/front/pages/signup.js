dcb506266bebe24228573d6f83a2c926d3706fc5
ineggapps <inegg.apps@gmail.com>
2019-11-05 23:56:32 +0900

    회원가입 페이지 유효성 체크

--- ch1/front/pages/signup.js
+++ ch1/front/pages/signup.js
@@ -9,7 +9,21 @@ const Signup = () => {
   const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
 
-  const onSubmit = () => {};
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+
+    console.log(id, nickname, password, passwordCheck, term);
+  };
   const onChangeId = e => {
     setId(e.target.value);
   };
@@ -20,11 +34,14 @@ const Signup = () => {
     setPassword(e.target.value);
   };
   const onChangePasswordChk = e => {
+    setPasswordError(e.target.value !== password);
     setPasswordCheck(e.target.value);
   };
   const onChangeTerm = e => {
-    setTerm(e.target.value);
+    setTermError(false);
+    setTerm(e.target.checked);
   };
+
   return (
     <AppLayout>
       <Form onSubmit={onSubmit} style={{ padding: 10 }}>
@@ -59,13 +76,17 @@ const Signup = () => {
             value={passwordCheck}
             onChange={onChangePasswordChk}
           ></Input>
+          {passwordError && (
+            <p style={{ color: "red" }}>Password Confirm is different from password input box.</p>
+          )}
         </div>
         <div>
           <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
             I agree
           </Checkbox>
+          {termError && <p style={{ color: "red" }}>You must check on the checkbox</p>}
         </div>
-        <div>
+        <div style={{ marginTop: 10 }}>
           <Button type="primary" htmlType="submit">
             Register
           </Button>
