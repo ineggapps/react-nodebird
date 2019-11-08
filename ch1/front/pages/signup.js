cca65f281a1dfa9e91c05b0a3130cf98b519ae1f
ineggapps <inegg.apps@gmail.com>
2019-11-09 00:19:59 +0900

    useInput 컴포넌트 별도의 파일로 구성하였음

--- ch1/front/pages/signup.js
+++ ch1/front/pages/signup.js
@@ -1,27 +1,12 @@
 import React, { useState, useCallback } from "react";
-import PropTypes from "prop-types";
 import { Form, Input, Checkbox, Button } from "antd";
-
-const TextInput = ({ value }) => {
-  return <div>{value}</div>;
-};
-
-TextInput.propTypes = {
-  value: PropTypes.string
-};
+import { useInput } from "../components/UseInput";
 
 const Signup = () => {
   const [passwordCheck, setPasswordCheck] = useState("");
   const [term, setTerm] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [termError, setTermError] = useState(false);
-  const useInput = (initValue = null) => {
-    const [value, setter] = useState(initValue);
-    const handler = useCallback(e => {
-      setter(e.target.value);
-    }, []);
-    return [value, handler];
-  };
 
   const [id, onChangeId] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
@@ -55,7 +40,6 @@ const Signup = () => {
   return (
     <>
       <Form onSubmit={onSubmit} style={{ padding: 10 }}>
-        <TextInput value="dsfd" />
         <div>
           <label htmlFor="user-id">Id</label>
           <br />
