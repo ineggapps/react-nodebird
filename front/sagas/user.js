57412d4bc955e3a6b60c9b6392601ee036caf9bc
ineggapps <inegg.apps@gmail.com>
2019-11-21 22:12:20 +0900

    takeLatest와 takeEvery 실습

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,6 +1,7 @@
-import { all, fork, takeLatest, call, put, take } from "redux-saga/effects";
+import { all, fork, takeEvery, takeLatest, call, put, take } from "redux-saga/effects";
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../reducers/user";
 
+const HELLO_SAGA = "HELLO_SAGA";
 function loginAPI() {
   //서버에 요청을 보내는 부분
 }
@@ -30,8 +31,27 @@ function* watchLogin() {
   }
 }
 
+function* watchHello() {
+  yield takeEvery(HELLO_SAGA, function*() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+  });
+}
+
+// function* watchHello() {
+//   while (true) {
+//     yield take(HELLO_SAGA);
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+//   }
+// }
+
 function* watchSignUp() {}
 
 export default function* userSaga() {
-  yield all([watchLogin(), watchSignUp()]);
+  yield all([watchLogin(), watchSignUp(), watchHello()]);
 }
