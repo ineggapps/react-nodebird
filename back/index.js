a541d350b37600a35373cb884e77e02bf69d97a6
ineggapps <inegg.apps@gmail.com>
2019-12-03 19:48:38 +0900

    회원가입 컨트롤러 만들기

--- back/index.js
+++ back/index.js
@@ -4,9 +4,13 @@ const userAPIRouter = require("./routes/user");
 const postAPIRouter = require("./routes/post");
 const postsAPIRouter = require("./routes/posts");
 const app = express();
+db.sequelize.sync();
 const port = 3065;
+//DB 구성하기
 
-// db.sequelize.sync();
+//JSON 형식의 본문을 처리하기 위해서 필요한 구문들
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //API: 다른 서비스가 나의 서비스의 기느응ㄹ 실행할 수 있도록 열여 둔 창구
 app.use("/api/user", userAPIRouter);
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const db = require("../models");
 const router = express.Router();
 
 router.get("/", (req, res) => {});
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  //ghldnjsrkdlq
+  try {
+    const exUser = await db.User.findOne({
+      where: {
+        userId: req.body.userId
+      }
+    });
+    if (exUser) {
+      return res.status(403).send("It is already used.");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12); //salt는 10~12 사이에서 만듦.
+    const newUser = await db.User.create({
+      nickname: req.body.nickname,
+      userId: req.body.userId,
+      password: hashedPassword
+    });
+    console.log(newUser);
+    return res.json(newUser);
+  } catch (e) {
+    console.error(e);
+    // return res.status(403).send(e);
+    return next(e); //프런트에 알아서 오류가 났다고 알려 줌.
+  }
+});
 router.get("/:id", (req, res) => {
   //타인의 정보 가져오기 (req.param.id)
 });
