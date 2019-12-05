315481241b037346066ad550e9dc0e59ef97f909
ineggapps <inegg.apps@gmail.com>
2019-12-05 22:09:02 +0900

    데이터베이스에서 유저의 Post, Followings, Followers 정보 가져옴

--- back/passport/index.js
+++ back/passport/index.js
@@ -22,9 +22,26 @@ module.exports = () => {
     */
     try {
       const user = await db.User.findOne({
-        where: { id }
+        where: { id },
+        include: [
+          {
+            model: db.Post,
+            as: "Posts",
+            attributes: ["id"]
+          },
+          {
+            model: db.User,
+            as: "Followings",
+            attributes: ["id"]
+          },
+          {
+            model: db.User,
+            as: "Followers",
+            attributes: ["id"]
+          }
+        ]
       });
-      return done(null, user);
+      return done(null, user); //req.user
     } catch (e) {
       console.error(e);
       return done(e);
