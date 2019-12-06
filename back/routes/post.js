d818e21d02b7637f59ef61a4bdf30c482e9dbe62
ineggapps <inegg.apps@gmail.com>
2019-12-06 20:46:15 +0900

    글 작성 API

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,7 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res, next) => {
+  //api/post
+  try {
+    const hashtags = req.body.content.match(/#[^\s]+/g);
+    const newPost = await db.Post.create({
+      // Hello World #subscribe #Like #Click
+      content: req.body.content,
+      UserId: req.user.id
+    });
+    if (hashtags) {
+      const result = await Promise.all(
+        hashtags.map(tag =>
+          db.Hashtag.findOrCreate({
+            //없으면 넣고 있으면 아무 것도 하지 않음
+            where: { name: tag.slice(1).toLowerCase() }
+          })
+        )
+      );
+      console.log(result);
+      await newPost.addHashtags(result.map(r => r[0]));
+    }
+    //const User = await newPost.getUser();
+    //newPost.user = User;
+    //res.json(newPost);
+    const fullPost = await db.Post.findOne({
+      where: { id: newPost.id },
+      include: [
+        {
+          model: db.User
+        }
+      ]
+    });
+    res.json(fullPost);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
 router.post("/images", (req, res) => {});
 
 module.exports = router;
