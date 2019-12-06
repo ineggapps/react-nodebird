3c5f9d22a7b235289abf7c3cb6a055b3c39ef3e4
ineggapps <inegg.apps@gmail.com>
2019-12-06 21:09:31 +0900

    작성되어 있는 게시글을 피드로 가져오기 위한 API 구현 (/api/posts)

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const db = require("../models");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res, next) => {
+  //GET api/posts
+  try {
+    const posts = await db.Post.findAll({
+      include: [
+        {
+          model: db.User,
+          attributes: ["id", "nickname"] //비밀번호와 같은 보안 항목은 가져오지 않기
+        }
+      ],
+      order: [
+        ["createdAt", "DESC"],
+        ["updatedAt", "ASC"]
+      ] //정렬
+    });
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
 
 module.exports = router;
